ad41d2c4e637be7ea54817e306214b89ddbd40c1
xXAgentMUXx <mathys.urban@ynov.com>
2025-03-20 19:44:06 +0100

    Bug Fix Order and Cart

--- demo/src/main/java/com/example/demo/dto/CartRequest.java
+++ demo/src/main/java/com/example/demo/dto/CartRequest.java
@@ -12,14 +12,22 @@ public class CartRequest {
         this.productId = productId;
         this.quantity = quantity;
     }
-
-
-    public Long getUserId() { return userId; }
-    public void setUserId(Long userId) { this.userId = userId; }
-
-    public Long getProductId() { return productId; }
-    public void setProductId(Long productId) { this.productId = productId; }
-
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public Long getUserId() { 
+        return userId; 
+    }
+    public void setUserId(Long userId) { 
+        this.userId = userId; 
+    }
+    public Long getProductId() { 
+        return productId; 
+    }
+    public void setProductId(Long productId) { 
+        this.productId = productId; 
+    }
+    public int getQuantity() { 
+        return quantity; 
+    }
+    public void setQuantity(int quantity) { 
+        this.quantity = quantity; 
+    }
 }
--- demo/src/main/java/com/example/demo/dto/OrderRequest.java
+++ demo/src/main/java/com/example/demo/dto/OrderRequest.java
@@ -14,16 +14,22 @@ public class OrderRequest {
         this.productIds = productIds;
         this.quantities = quantities;
     }
-
-    public Long getUserId() { return userId; }
-    public void setUserId(Long userId) { this.userId = userId; }
-
-    public List<Long> getProductIds() { return productIds; }
-    public void setProductIds(List<Long> productIds) { this.productIds = productIds; }
-    public List<Integer> getQuantities() {
-        return quantities;
+    public Long getUserId() { 
+        return userId; 
     }
-    public void setQuantities(List<Integer> quantities) {
-        this.quantities = quantities;
+    public void setUserId(Long userId) { 
+        this.userId = userId; 
+    }
+    public List<Long> getProductIds() { 
+        return productIds; 
+    }
+    public void setProductIds(List<Long> productIds) { 
+        this.productIds = productIds; 
+    }
+    public List<Integer> getQuantities() { 
+        return quantities; 
+    }
+    public void setQuantities(List<Integer> quantities) { 
+        this.quantities = quantities; 
     }
 }
